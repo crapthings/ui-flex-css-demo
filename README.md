2d705088ab21c1df49721374a368a83718997aee
crapthings <crapthings@gmail.com>
2016-11-16 12:43:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ui-flex-css-demo
---
+++ client/helpers.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'
+import faker from 'faker'
+import randomcolor from 'randomcolor'
+
+Template.registerHelper('rc', function () {
+  return randomcolor()
+})
+
+Template.registerHelper('list', function(length = 50) {
+  return _.times(length, n => ({
+    title: faker.lorem.sentence(),
+    content: faker.lorem.paragraphs(),
+  }))
+})
